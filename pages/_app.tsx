16e6efaa2b836ed41b5cf70043f3b1f525655de9
maxime beuil <maxime.beuil@algoan.com>
2021-09-04 22:26:37 +0200

    test using material ui and a theme in a basic app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {ThemeProvider, createTheme} from '@material-ui/core';
+import type {AppProps} from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
+  const darkTheme = createTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-export default MyApp
+export default MyApp;
