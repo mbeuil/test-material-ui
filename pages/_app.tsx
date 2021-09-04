c2d37c7514717c917773eca2c250731879179eea
maxime beuil <maxime.beuil@algoan.com>
2021-09-04 22:50:31 +0200

    fix material ui + styled components ssr className

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,13 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import {ThemeProvider, createTheme} from '@material-ui/core';
 import type {AppProps} from 'next/app';
 
-function MyApp({Component, pageProps}: AppProps): JSX.Element {
-  const darkTheme = createTheme({
-    palette: {
-      type: 'dark',
-    },
-  });
+export const darkTheme = createTheme({
+  palette: {
+    type: 'dark',
+  },
+});
 
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,13 @@
-import Document, {DocumentContext} from 'next/document';
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
 import {ServerStyleSheet} from 'styled-components';
+import {ServerStyleSheets} from '@material-ui/core/styles';
+import {darkTheme} from './_app';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<{
@@ -7,14 +15,18 @@ class MyDocument extends Document {
     html: string;
     head?: (JSX.Element | null)[] | undefined;
   }> {
-    const sheet = new ServerStyleSheet();
+    const styledComponentsSheet = new ServerStyleSheet();
+    const materialSheets = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           // eslint-disable-next-line react/display-name
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+          enhanceApp: App => props =>
+            styledComponentsSheet.collectStyles(
+              materialSheets.collect(<App {...props} />),
+            ),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
@@ -23,13 +35,36 @@ class MyDocument extends Document {
         styles: (
           <>
             {initialProps.styles}
-            {sheet.getStyleElement()}
+            {materialSheets.getStyleElement()}
+            {styledComponentsSheet.getStyleElement()}
           </>
         ),
       };
     } finally {
-      sheet.seal();
+      styledComponentsSheet.seal();
     }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta name="theme-color" content="#0d1219" />
+          <meta name="color-scheme" content="dark light" />
+          {/* PWA primary color */}
+          <meta name="theme-color" content={darkTheme.palette.primary.main} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
 
